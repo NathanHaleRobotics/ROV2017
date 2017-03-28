64750923a637eebadc98bc9aa87dfcd2fae65bb1
Solomon Ritzow <solduck@gmail.com>
2017-03-27 22:47:57 -0700

    Set up JInput and made a small demo.
    JInput demo in StartUI class prints out all controllers and their
    components. The jinput library is located in lib/jinput and contains the
    native libraries, source code, and library jar.

--- src/nathanhale/robotics/main/StartUI.java
+++ src/nathanhale/robotics/main/StartUI.java
@@ -1,7 +1,15 @@
 package nathanhale.robotics.main;
 
+import net.java.games.input.*;
+
 public class StartUI {
 	public static void main(String... args) {
-		
+		for(Controller controller : ControllerEnvironment.getDefaultEnvironment().getControllers()) {
+			System.out.println(controller.getName() + ":");
+			for(Component component : controller.getComponents()) {
+				System.out.println(" - " + component.getName());
+			}
+			System.out.println();
+		}
 	}
 }
