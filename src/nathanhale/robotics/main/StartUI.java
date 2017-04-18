4e133dfe030cd7251acc796383a2e170d707124e
Solomon Ritzow <solduck@gmail.com>
2017-04-17 22:29:34 -0700

    Removed jSSC, added sample jSerialComm code and src
    Also removed custom SerialPortInputStream, etc.

--- src/nathanhale/robotics/main/StartUI.java
+++ src/nathanhale/robotics/main/StartUI.java
@@ -16,11 +16,10 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-import jssc.SerialPort;
-import jssc.SerialPortException;
 import net.java.games.input.Component;
 import net.java.games.input.Controller;
 import net.java.games.input.ControllerEnvironment;
+import com.fazecast.jSerialComm.*;
 
 /**
  * The main class of the ROV Controller, which begins the user interface and other communication components
@@ -28,6 +27,11 @@ import net.java.games.input.ControllerEnvironment;
  *
  */
 public class StartUI extends Application {
+	
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+	
 	public void start(Stage stage) throws Exception {
 
 		//in JavaFX, windows are called stages
@@ -46,18 +50,34 @@ public class StartUI extends Application {
 
 		//create a scrollbar area
 		VBox root = new VBox();
-		Tab infoTab = new Tab("Devices"), controlsTab = new Tab("Controls");
-		infoTab.setClosable(false);
+		Tab controlsTab = new Tab("Controls"), devicesTab = new Tab("Device List");
+		devicesTab.setClosable(false);
 		controlsTab.setClosable(false);
-		TabPane tabs = new TabPane(infoTab, controlsTab);
+		TabPane tabs = new TabPane(controlsTab, devicesTab);
 		root.getChildren().add(tabs);
 		//scenes are the topmost container inside a stage, they can be swapped out
 		Scene scene = new Scene(root);
 		scene.setFill(Color.BLUE); //so we can tell when there isn't anything filling the scene
 		stage.setScene(scene);
 		
-		//load all the the JInput controller names and components into a String (unrelated to JavaFX code above)
+		//create a string builder for all of the serial port and input device information
 		StringBuilder sb = new StringBuilder();
+		SerialPort arduino = null;
+		
+		//write the list of serial ports to the string builder
+		sb.append("Serial Ports:\n");
+		for(SerialPort port : SerialPort.getCommPorts()) {
+			sb.append('\t');
+			sb.append(port.getDescriptivePortName());
+			sb.append('\n');
+			if(port.getDescriptivePortName().contains("rduino")) { //so that capitalization of 'a' doesn't matter
+				port.openPort();
+				arduino = port;
+			}
+		}
+		
+		//load all the the JInput controller names and components into the string builder (unrelated to JavaFX code above)
+		sb.append("Input Devices:\n");
 		for(Controller controller : ControllerEnvironment.getDefaultEnvironment().getControllers()) {
 			sb.append(controller.getName());
 			sb.append(":\n");
@@ -73,22 +93,27 @@ public class StartUI extends Application {
 		devices.setFont(Font.font("consolas", 16));
 		ScrollPane sp = new ScrollPane();
 		sp.setContent(devices);
-		infoTab.setContent(sp);
+		devicesTab.setContent(sp);
 		
 		//show the window
 		stage.show();
-	}
-	
-	SerialPort getSerialPort() {
-		for(int i = 1; i <= 10; i++) {
-			try {
-				SerialPort port = new SerialPort("COM" + i);
-				port.openPort();
-				return port;
-			} catch(SerialPortException e) {
-				continue;
-			}
-		}
-		return null;
+		
+//		if(arduino != null) {
+//			int value = 100;
+//			byte[] fourBytes = new byte[4];
+//			fourBytes[0] = (byte)(value >>> 24);
+//			fourBytes[1] = (byte)(value >>> 16);
+//			fourBytes[2] = (byte)(value >>> 8);
+//			fourBytes[3] = (byte)(value >>> 0);
+//			arduino.getOutputStream().write(fourBytes);
+//		}
+		
+//		byte[] packet = new byte[100];
+//		while(true) {
+//			arduino.getInputStream().read(packet);
+//			System.out.print(new String(packet));
+//		}
+		
+		arduino.closePort(); //just temporary
 	}
 }
